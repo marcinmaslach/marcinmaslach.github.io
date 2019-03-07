03caffd3812afe115dde61d99b271f6b75d421d4
Marcin Maślach <45828153+marcinmaslach@users.noreply.github.com>
2019-03-07 22:54:49 +0100

    Add files via upload
    Add jQuery with better hover images

---
+++ myscript.js
@@ -0,0 +1,53 @@
+$("#assistant").on({
+ "mouseover" : function() {
+    this.src = "assistant2.jpg";
+  },
+  "mouseout" : function() {
+    this.src= "assistant.jpg";
+  }
+});
+
+$("#crawler").on({
+ "mouseover" : function() {
+    this.src = "webcrawler2.jpg";
+  },
+  "mouseout" : function() {
+    this.src= "webcrawler.jpg";
+  }
+});
+
+$("#shiny").on({
+ "mouseover" : function() {
+    this.src = "quanto2.jpg";
+  },
+  "mouseout" : function() {
+    this.src= "quanto.jpg";
+  }
+});
+
+$("#enigma").on({
+ "mouseover" : function() {
+    this.src = "enigma2.jpg";
+  },
+  "mouseout" : function() {
+    this.src= "enigma.jpg";
+  }
+});
+
+$("#mac").on({
+ "mouseover" : function() {
+    this.src = "macchanger2.jpg";
+  },
+  "mouseout" : function() {
+    this.src= "macchanger.jpg";
+  }
+});
+
+$("#scanner").on({
+ "mouseover" : function() {
+    this.src = "webscanner2.jpg";
+  },
+  "mouseout" : function() {
+    this.src= "webscanner.jpg";
+  }
+});
